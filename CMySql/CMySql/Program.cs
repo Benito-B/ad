0a51d3314083a79976f4d7e9612a5f837adecdb8
BenitoB <benitobarreiro89@gmail.com>
2019-09-23 12:19:41 +0200

    Añadido proyecto MySql y mysqlconnector

---
+++ CMySql/CMySql/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using MySql.Data.MySqlClient;
+
+namespace CMySql
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Acceso a dbprueba con MySql");
+            MySqlConnection conn = new MySqlConnection("server=localhost;database=dbpruebas;user=root;password=sistemas;ssl-mode=none");
+            conn.Open();
+        }
+    }
+}
