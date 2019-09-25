dd2859f25816b02b4f081f6e96d2bb83c518e7de
BenitoB <benitobarreiro89@gmail.com>
2019-09-25 13:57:51 +0200

    mysql proyect changes

--- CMySql/CMySql/Program.cs
+++ CMySql/CMySql/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Data;
 using MySql.Data.MySqlClient;
 
 namespace CMySql
@@ -8,8 +9,15 @@ namespace CMySql
         public static void Main(string[] args)
         {
             Console.WriteLine("Acceso a dbprueba con MySql");
-            MySqlConnection conn = new MySqlConnection("server=localhost;database=dbpruebas;user=root;password=sistemas;ssl-mode=none");
-            conn.Open();
+            IDbConnection dbConn = new MySqlConnection("server=localhost;database=dbpruebas;user=root;password=sistemas;ssl-mode=none");
+            dbConn.Open();
+
+            IDbCommand dbCommand = dbConn.CreateCommand();
+
+            dbCommand.CommandText = "select * from categoria";
+            IDataReader dataReader = dbCommand.ExecuteReader();
+
+            dbConn.Close();
         }
     }
 }
