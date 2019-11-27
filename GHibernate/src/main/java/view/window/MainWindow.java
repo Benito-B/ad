da55437e690d208ca5f1dc399d548d75bddd48ba
BenitoB <benitobarreiro89@gmail.com>
2019-11-27 11:57:02 +0100

    Commentary changes

--- GHibernate/src/main/java/view/window/MainWindow.java
+++ GHibernate/src/main/java/view/window/MainWindow.java
@@ -7,14 +7,20 @@ import java.awt.event.WindowEvent;
 
 public class MainWindow extends JFrame {
 
+    /**
+     * Constructor de la ventana. La inicia, le setea los valores por defecto y la pone visible.
+     * Está a pantalla completa y si se consigue cambiar, estará con un tamaño que ocupa el espacio disponible
+     */
     public MainWindow(){
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
         this.setAlwaysOnTop(true);
         this.setUndecorated(true);
         this.setExtendedState(JFrame.MAXIMIZED_BOTH);
+        //Cojo el entorno para poder obtener los valores de tamaño de la pantalla y se los seteo a la ventana
         GraphicsEnvironment env = GraphicsEnvironment.getLocalGraphicsEnvironment();
         this.setMaximizedBounds(env.getMaximumWindowBounds());
         this.setSize(env.getMaximumWindowBounds().width, env.getMaximumWindowBounds().height);
+        //También cojo el GraphicsDevice que me permite mostrar la ventana como fullscreen
         GraphicsDevice device = GraphicsEnvironment.getLocalGraphicsEnvironment().getDefaultScreenDevice();
         device.setFullScreenWindow(this);
         this.setVisible(true);
